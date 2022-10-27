c14aa49a2725b5b33dbff6e8d6f7a8a7d8c0ac41
Edanur <Edanur@LAPTOP-LKCP4O85>
2022-10-27 10:16:43 +0300

    Second

--- Diziler/src/arrays/arrays.java
+++ Diziler/src/arrays/arrays.java
@@ -2,6 +2,35 @@ package arrays;
 
 public class arrays {
 	public static void main (String[] args) {
+		/*int[] array = new int[10];
+		
+		System.out.printf("%s%8s%n","Index", "Value");
+		
+		for(int counter = 0; counter < array.length; counter++) {
+			System.out.printf("%5d%8d%n", counter, array[counter]);*/
+				
+		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+		
+		System.out.printf("%s%8s%n", "Index", "Value");
+		
+		for (int counter = 0; counter < array.length; counter++) {
+			System.out.printf("%5d%8d%n", counter, array[counter] );
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+		
 		
 	}
 	
